c1c87ed6a6f5cf74433b6d48bb3bde7270e2d74a
中下游 <287276293@qq.com>
2026-07-01 00:16:11 +0800

    fix: 修复UIInstaller初始化UIManager时参数遗漏。

--- Runtime/Essentials.cs
+++ Runtime/Essentials.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using VContainer;
 using VContainer.Unity;
+using static UnityEditor.ObjectChangeEventStream;
 
 namespace SUG.Essentials
 {
@@ -14,15 +15,21 @@ namespace SUG.Essentials
         {
             if (Resolver != null) return;
 
-            // 找到这个项目中唯一的配置文件
-            Settings = Resources.Load<EssentialsSettingsSO>("Essentials/Bootstrap");
-            if (Settings == null) { Debug.LogError("[Essentials] Settings not found in Resources!"); return; }
-
             // 组件Installer注册
             var builder = new ContainerBuilder();
             RuntimeBridge.Initialize();
-            UIInstaller.Install(builder, Settings.uiSetting);
+
+            UIInitializaction(builder);
+
             Resolver = builder.Build();
+        }
+
+        private static void UIInitializaction(ContainerBuilder builder)
+        {
+            // 找到这个项目中唯一的配置文件
+            Settings = Resources.Load<EssentialsSettingsSO>("Essentials/Bootstrap");
+            if (Settings == null) { Debug.LogError("[Essentials] Settings not found in Resources!"); return; }
+            UIInstaller.Install(builder, Settings.uiSetting);
         }
     }
 }
--- Runtime/UI/UIInstaller.cs
+++ Runtime/UI/UIInstaller.cs
@@ -7,6 +7,7 @@ namespace SUG.Essentials
     {
         public static void Install(IContainerBuilder builder, UISettingsSO settings)
         {
+            builder.RegisterInstance(settings);
             builder.RegisterInstance(RuntimeBridge.UI);
             builder.Register<IUIService, UIManager>(Lifetime.Singleton);
         }
